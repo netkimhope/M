f38bccb1d98a8f73e9e37eb95e77c89e4d461e1d
Churchill <141407524+Churchillabing1@users.noreply.github.com>
2024-05-29 13:27:13 +0800

    Update prefix.js

--- script/prefix.js
+++ script/prefix.js
@@ -32,7 +32,7 @@ module.exports.run = function ({ api, event, args, prefix, admin }) {
     if (userPrefix) {
         messageBody = `Yo, my prefix is [ 𓆩 ${userPrefix} 𓆪 ]\n\n𝗦𝗢𝗠𝗘 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 𝗧𝗛𝗔𝗧 𝗠𝗔𝗬 𝗛𝗘𝗟𝗣 𝗬𝗢𝗨:\n➥ ${userPrefix}➥ ${userPrefix}sim [message] -> talk to bot\n➥ ${userPrefix}➥ ${userPrefix}help [command] -> information and usage of command\n\nHave fun using it, enjoy! ❤️\nBot Developer: https://www.facebook.com/${admin}`;
     } else {
-        messageBody = `I don't have a prefix set.\n\n𝗦𝗢𝗠𝗘 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 𝗧𝗛𝗔𝗧 𝗠𝗔𝗬 𝗛𝗘𝗟𝗣 𝗬𝗢𝗨:\n➥ sim [message] -> talk to bot\n➥ callad [message] -> ➥ ai [command] -> can answer any question\n➥ help [command] -> information and usage of command\n\nHave fun using it, enjoy! ❤️\nBot Developer: https://www.facebook.com/${admin}`;
+        messageBody = `I don't have a prefix set.\n\n𝗦𝗢𝗠𝗘 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 𝗧𝗛𝗔𝗧 𝗠𝗔𝗬 𝗛𝗘𝗟𝗣 𝗬𝗢𝗨:\n➥ sim [message] -> talk to bot\n➥ ai [command] -> can answer any question\n➥ help [command] -> information and usage of command\n\nHave fun using it, enjoy! ❤️\nBot Developer: https://www.facebook.com/${admin}`;
     }
 
     api.sendMessage(

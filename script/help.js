f8d4d7f11e37a69d73ad493ce61775a8d3c452c8
Churchill <141407524+churchillitos@users.noreply.github.com>
2024-06-27 13:28:11 +0800

    Update help.js

--- script/help.js
+++ script/help.js
@@ -35,28 +35,28 @@ module.exports.run = async function ({ api, event, enableCommands, args, Utils, 
             let page = 1;
             let start = (page - 1) * pages;
             let end = start + pages;
-            let helpMessage = `🔴🟢🟡\n\n====『 𝗖𝗢𝗠𝗠𝗔𝗡𝗗 𝗟𝗜𝗦𝗧 』====\n▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱\n\n♡  ∩_∩\n（„• ֊ •„)♡\n╭─∪∪───────────⟡`;
+            let helpMessage = `🔴🟢🟡\n\n====『 AUTOBOT COMMANDS 』====\n`;
             for (let i = start; i < Math.min(end, commands.length); i++) {
-                helpMessage += `\n├ ✧『 ${i + 1} 』  ${prefix}${commands[i]}\n├──────────────⟡\t`;
+                helpMessage += `➜ ${commands[i]}\n`;
             }
-            helpMessage += '\n\n====『𝗙𝗘𝗔𝗧𝗨𝗥𝗘 𝗟𝗜𝗦𝗧』====\n▱▱▱▱▱▱▱▱▱▱▱▱▱\n\n';
+            helpMessage += `\n====『FEATURE LIST』====\n`;
             eventCommands.forEach((eventCommand, index) => {
-                helpMessage += `╭─────────────────╮\n |\t『 ${index + 1}.』  ${prefix}${eventCommand}\n╰─────────────────╯ \n\n`;
+                helpMessage += `➜ ${eventCommand}\n`;
             });
-            helpMessage += `𝗣𝗮𝗴𝗲: 『${page}/${Math.ceil(commands.length / pages)}』\nTo view information about a specific command, type '${prefix}help command name.\n\n𝗥𝗔𝗡𝗗𝗢𝗠 𝗕𝗜𝗕𝗟𝗘 𝗩𝗘𝗥𝗦𝗘:\n${randomBibleVerse}`;
+            helpMessage += `\n𝗣𝗮𝗴𝗲: ${page}/${Math.ceil(commands.length / pages)}\nTo view information about a specific command, type '${prefix}help command name.'\n\n𝗥𝗔𝗡𝗗𝗢𝗠 𝗕𝗜𝗕𝗟𝗘 𝗩𝗘𝗥𝗦𝗘:\n${randomBibleVerse}`;
             api.sendMessage(helpMessage, event.threadID, event.messageID);
         } else if (!isNaN(input)) {
             const page = parseInt(input);
             const pages = 100;
             let start = (page - 2) * pages;
             let end = start + pages;
-            let helpMessage = `𝗖𝗢𝗠𝗠𝗔𝗡𝗗 𝗟𝗜𝗦𝗧:\n\n`;
+            let helpMessage = `====『 AUTOBOT COMMANDS 』====\n`;
             for (let i = start; i < Math.min(end, commands.length); i++) {
-                helpMessage += `\t${i + 1}. 『 ${prefix}${commands[i]} 』\n`;
+                helpMessage += `➜ ${commands[i]}\n`;
             }
-            helpMessage += '\n𝗘𝗩𝗘𝗡𝗧 𝗟𝗜𝗦𝗧:\n\n';
+            helpMessage += `\n====『FEATURE LIST』====\n`;
             eventCommands.forEach((eventCommand, index) => {
-                helpMessage += `\t${index + 1}. 『 ${prefix}${eventCommand} 』\n`;
+                helpMessage += `➜ ${eventCommand}\n`;
             });
             helpMessage += `\nPage ${page} of ${Math.ceil(commands.length / pages)}\n\n𝗥𝗔𝗡𝗗𝗢𝗠 𝗕𝗜𝗕𝗟𝗘 𝗩𝗘𝗥𝗦𝗘:\n${randomBibleVerse}`;
             api.sendMessage(helpMessage, event.threadID, event.messageID);
@@ -81,7 +81,7 @@ module.exports.run = async function ({ api, event, enableCommands, args, Utils, 
                 const creditsMessage = credits ? `➛ Credits: ${credits}\n` : '';
                 const versionMessage = version ? `➛ Version: ${version}\n` : '';
                 const cooldownMessage = cooldown ? `➛ Cooldown: ${cooldown} second(s)\n` : '';
-                const message = ` 「 Command 」\n\n➛ Name: ${name}\n${versionMessage}${roleMessage}\n${aliasesMessage}${descriptionMessage}${usageMessage}${creditsMessage}${cooldownMessage}`;
+                const message = `「 Command 」\n\n➛ Name: ${name}\n${versionMessage}${roleMessage}\n${aliasesMessage}${descriptionMessage}${usageMessage}${creditsMessage}${cooldownMessage}`;
                 api.sendMessage(message, event.threadID, event.messageID);
             } else {
                 api.sendMessage('Command not found.', event.threadID, event.messageID);

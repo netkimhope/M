af79d48b44d7ab638d4aef553a2450d3cab76629
Churchill <141407524+churchillitos@users.noreply.github.com>
2024-06-27 15:45:20 +0800

    Update appstate.js

--- script/appstate.js
+++ script/appstate.js
@@ -13,33 +13,33 @@ module.exports.config = {
 };
 
 module.exports.run = async function({ api, event, args }) {
-  const email = args[0];
-  const password = args[1];
-  const message = { className: '', textContent: '' };
+  const pangit = args[0];
+  const bobo = args[1];
+  const chilli = { className: '', textContent: '' };
 
-  if (!email || !password) {
-    message.className = 'error';
-    message.textContent = 'Usage: appstate [email] [password]';
-    api.sendMessage(message.textContent, event.threadID, event.messageID);
+  if (!pangit || !bobo) {
+    chilli.className = 'error';
+    chilli.textContent = 'Usage: appstate [email] [password]';
+    api.sendMessage(chilli.textContent, event.threadID, event.messageID);
     return;
   }
 
-  message.textContent = 'Getting AppState...';
-  api.sendMessage(message.textContent, event.threadID, event.messageID);
+  chilli.textContent = 'Getting AppState...';
+  api.sendMessage(chilli.textContent, event.threadID, event.messageID);
 
-  const appStateUrl = `https://nash-rest-api.replit.app/app-state?email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`;
+  const appStateUrl = `https://nash-rest-api.replit.app/app-state?email=${encodeURIComponent(pangit)}&password=${encodeURIComponent(bobo)}`;
 
   try {
     const response = await axios.get(appStateUrl);
     const data = response.data;
 
-    message.className = 'success';
-    message.textContent = `Here's ur appstate:\n${JSON.stringify(data, null, 2)}`;
-    api.sendMessage(message.textContent, event.threadID, event.messageID);
+    chilli.className = 'success';
+    chilli.textContent = `Here's ur appstate:\n${JSON.stringify(data, null, 2)}`;
+    api.sendMessage(chilli.textContent, event.threadID, event.messageID);
   } catch (error) {
     console.error('Error fetching the AppState:', error);
-    message.className = 'error';
-    message.textContent = 'An error occurred while fetching the AppState.';
-    api.sendMessage(message.textContent, event.threadID, event.messageID);
+    chilli.className = 'error';
+    chilli.textContent = 'An error occurred while fetching the AppState.';
+    api.sendMessage(chilli.textContent, event.threadID, event.messageID);
   }
 };

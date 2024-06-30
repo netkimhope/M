04eed532f306d9a83b716cfb1d864f63af3270f0
Churchill <141407524+churchillitos@users.noreply.github.com>
2024-06-30 11:23:46 +0800

    Update help.js

--- script/help.js
+++ script/help.js
@@ -1,89 +1,89 @@
 const axios = require('axios');
 
 module.exports.config = {
-    name: 'help',
-    version: '1.0.0',
-    role: 0,
-    hasPrefix: false,
-    aliases: ['help'],
-    description: "Beginner's guide",
-    usage: "Help [page] or [command]",
-    credits: 'Developer',
+  name: 'help',
+  version: '1.0.0',
+  role: 0,
+  hasPrefix: false,
+  aliases: ['help'],
+  description: "Beginner's guide",
+  usage: "Help [page] or [command]",
+  credits: 'Developer',
 };
 
 module.exports.run = async function ({ api, event, enableCommands, args, Utils, prefix }) {
-    const input = args.join(' ');
+  const input = args.join(' ');
 
-    try {
-        const eventCommands = enableCommands[1].handleEvent;
-        const commands = enableCommands[0].commands;
+  try {
+    const eventCommands = enableCommands[1].handleEvent;
+    const commands = enableCommands[0].commands;
 
-        const fetchRandomBibleVerse = async () => {
-            try {
-                const response = await axios.get('https://deku-rest-api-gadz.onrender.com/bible');
-                return `📖 ${response.data.verse}\n- ${response.data.reference}`;
-            } catch (error) {
-                console.error('Error fetching Bible verse:', error);
-                return 'An error occurred while fetching the Bible verse.';
-            }
-        };
+    const fetchRandomBibleVerse = async () => {
+      try {
+        const response = await axios.get('https://deku-rest-api-gadz.onrender.com/bible');
+        return `📖 ${response.data.verse}\n- ${response.data.reference}`;
+      } catch (error) {
+        console.error('Error fetching Bible verse:', error);
+        return 'An error occurred while fetching the Bible verse.';
+      }
+    };
 
-        const randomBibleVerse = await fetchRandomBibleVerse();
+    const randomBibleVerse = await fetchRandomBibleVerse();
 
-        const totalCommands = commands.length;
-        const pages = 3;
-        const commandsPerPage = Math.ceil(totalCommands / pages);
+    const totalCommands = commands.length;
+    const pages = Math.ceil(totalCommands / 15); // Adjust the number 15 to change commands per page
 
-        if (!input || !isNaN(input)) {
-            const page = input ? parseInt(input) : 1;
+    if (!input || !isNaN(input)) {
+      const page = input ? parseInt(input) : 1;
 
-            if (page < 1 || page > pages) {
-                return api.sendMessage(`Page ${page} does not exist. Please choose a page between 1 and ${pages}.`, event.threadID, event.messageID);
-            }
+      if (page < 1 || page > pages) {
+        return api.sendMessage(`Page ${page} does not exist. Please choose a page between 1 and ${pages}.`, event.threadID, event.messageID);
+      }
 
-            const start = (page - 1) * commandsPerPage;
-            const end = Math.min(start + commandsPerPage, totalCommands);
+      const start = (page - 1) * 15;
+      const end = Math.min(start + 15, totalCommands);
 
-            let helpMessage = `🔴🟢🟡\n\n『 𝑨𝑼𝑻𝑶𝑩𝑶𝑻 𝑪𝑶𝑴𝑴𝑨𝑵𝑫 𝑳𝑰𝑺𝑻 』\n`;
-            for (let i = start; i < end; i++) {
-                helpMessage += `➜ ${commands[i]}\n`;
-            }
+      let helpMessage = `━━𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂━━\n`;
+      for (let i = start; i < end; i++) {
+        helpMessage += ` ⊂⊃ ➥ ${commands[i]}✨\n`;
+      }
 
-            helpMessage += `\n====『FEATURE LIST』====\n`;
-            eventCommands.forEach((eventCommand) => {
-                helpMessage += `➜ ${eventCommand}\n`;
-            });
+      helpMessage += `━━━━━━━━━━━━━━━\n`;
+      helpMessage += `━━𝙲𝙾𝙼𝙼𝙰𝙽𝙳 𝙿𝙰𝙶𝙴 : <${page}/${pages}>━━\n`;
+      helpMessage += `━━CHILLI 𝖠𝖨 𝖢𝖧𝖠𝖳𝖡𝖮𝖳━━\n`;
+      helpMessage += `𝗥𝗔𝗡𝗗𝗢𝗠 𝗕𝗜𝗕𝗟𝗘 𝗩𝗘𝗥𝗦𝗘:\n${randomBibleVerse}`;
 
-            helpMessage += `\nPage: ${page}/${pages}\nTo view information about a specific command, type '${prefix}help command name.'\nTo see the available list of commands, type '${prefix}help [page number]'.\n\n𝗥𝗔𝗡𝗗𝗢𝗠 𝗕𝗜𝗕𝗟𝗘 𝗩𝗘𝗥𝗦𝗘:\n${randomBibleVerse}`;
-            api.sendMessage(helpMessage, event.threadID, event.messageID);
-        } else {
-            const command = [...Utils.handleEvent, ...Utils.commands].find(([key]) => key.includes(input?.toLowerCase()))?.[1];
-            if (command) {
-                const {
-                    name,
-                    version,
-                    role,
-                    aliases = [],
-                    description,
-                    usage,
-                    credits,
-                    cooldown,
-                    hasPrefix
-                } = command;
-                const roleMessage = role !== undefined ? (role === 0 ? '➛ Permission: user' : (role === 1 ? '➛ Permission: admin' : (role === 2 ? '➛ Permission: thread Admin' : (role === 3 ? '➛ Permission: super Admin' : '')))) : '';
-                const aliasesMessage = aliases.length ? `➛ Aliases: ${aliases.join(', ')}\n` : '';
-                const descriptionMessage = description ? `Description: ${description}\n` : '';
-                const usageMessage = usage ? `➛ Usage: ${usage}\n` : '';
-                const creditsMessage = credits ? `➛ Credits: ${credits}\n` : '';
-                const versionMessage = version ? `➛ Version: ${version}\n` : '';
-                const cooldownMessage = cooldown ? `➛ Cooldown: ${cooldown} second(s)\n` : '';
-                const message = `「 Command 」\n\n➛ Name: ${name}\n${versionMessage}${roleMessage}\n${aliasesMessage}${descriptionMessage}${usageMessage}${creditsMessage}${cooldownMessage}`;
-                api.sendMessage(message, event.threadID, event.messageID);
-            } else {
-                api.sendMessage('Command not found.', event.threadID, event.messageID);
-            }
-        }
-    } catch (error) {
-        console.log(error);
+      api.sendMessage(helpMessage, event.threadID, event.messageID);
+    } else {
+      const command = [...Utils.handleEvent, ...Utils.commands].find(([key]) => key.includes(input.toLowerCase()))?.[1];
+      if (command) {
+        const {
+          name,
+          version,
+          role,
+          aliases = [],
+          description,
+          usage,
+          credits,
+          cooldown,
+          hasPrefix
+        } = command;
+
+        const roleMessage = role !== undefined ? (role === 0 ? '➛ Permission: user' : (role === 1 ? '➛ Permission: admin' : (role === 2 ? '➛ Permission: thread Admin' : (role === 3 ? '➛ Permission: super Admin' : '')))) : '';
+        const aliasesMessage = aliases.length ? `➛ Aliases: ${aliases.join(', ')}\n` : '';
+        const descriptionMessage = description ? `Description: ${description}\n` : '';
+        const usageMessage = usage ? `➛ Usage: ${usage}\n` : '';
+        const creditsMessage = credits ? `➛ Credits: ${credits}\n` : '';
+        const versionMessage = version ? `➛ Version: ${version}\n` : '';
+        const cooldownMessage = cooldown ? `➛ Cooldown: ${cooldown} second(s)\n` : '';
+
+        const message = `「 Command 」\n\n➛ Name: ${name}\n${versionMessage}${roleMessage}\n${aliasesMessage}${descriptionMessage}${usageMessage}${creditsMessage}${cooldownMessage}`;
+        api.sendMessage(message, event.threadID, event.messageID);
+      } else {
+        api.sendMessage('Command not found.', event.threadID, event.messageID);
+      }
     }
+  } catch (error) {
+    console.log(error);
+  }
 };
